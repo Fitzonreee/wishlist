3c38cc71f586c87ebe1d2da8d25500eda4d96b3f
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2015-10-27 11:10:04 -0700

    initial commit

---
+++ application/views/leaderboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Leaderboard</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	</head>
+	<body>
+		<nav>
+		    <div class="nav-wrapper">
+		      <a href="#" class="brand-logo center">Wishlist</a>
+		      <ul id="nav-mobile" class="left hide-on-med-and-down">
+		        <li><a href="sass.html">Sass</a></li>
+		        <li><a href="badges.html">Components</a></li>
+		        <li><a href="collapsible.html">JavaScript</a></li>
+		      </ul>
+		    </div>
+		</nav>
+		<div class="container">
+			<h1>This is it</h1>
+		</div> <!-- end of container -->
+	</body>
+</html>
---
+++ application/views/partials/nav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Leaderboard</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	</head>
+	<body>
+		<nav>
+		    <div class="nav-wrapper">
+		      <a href="#" class="brand-logo">Pick 'em</a>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		        <li><a href="#">This Week</a></li>
+		        <li><a href="#">Leaderboard</a></li>
+		        <li><a href="#">Contact</a></li>
+		      </ul>
+		    </div>
+		</nav>
+		<div class="container">
+			<h1>This is it</h1>
+		</div> <!-- end of container -->
+	</body>
+</html>
