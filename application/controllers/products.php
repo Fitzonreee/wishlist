dd020bfb67457d8305f4246efef016c9ea4011c4
Ryan Tang <ryantang333@gmail.com>
2015-10-29 10:11:04 -0700

    moved views into view controllers...again.

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -2,23 +2,6 @@
 
 class Products extends CI_Controller {
 
-	public function index(){
-		$this->session->set_userdata('id', 1);
-		$data['products'] = $this->Product->get_all($this->input->get('id'));
-		$this->load->view('temp_view', $data);
-	}
-	public function home(){
-		$this->load->view('preferences');
-	}
-
-
-	public function add(){
-		echo "hello add";
-	}
-
-	public function info($id){
-		$item['info'] = $this->product->get_product($id);
-		$this->load->view('product_info', $item);
-	}
+	
 
 }
